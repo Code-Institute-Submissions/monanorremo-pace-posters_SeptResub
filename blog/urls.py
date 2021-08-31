d83e32ae95edb0ad1b245a4888f43a439be81be0
monanorremo <mona.norremo@gmail.com>
2021-08-31 18:41:45 +0000

    Pylinted products app

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('article/<int:pk>', PostDetailView.as_view(), name='post_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>/', EditPostView.as_view(), name='edit_post'),
-    path('article/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post')
-    ]
+    path('article/<int:pk>/delete/', DeletePostView.as_view(), name='delete_post')]
